a7d059b2d962bf887f13f6c8c4af2979968b8570
annjinette <ann.hess@rackspace.com>
2014-10-20 21:58:17 -0500

    change filename

--- run_analysis.R
+++ run_analysis.R
@@ -1,97 +1,77 @@
 #GETTING & CLEANING DATA -- COURSE PROJECT
-
-# You should create one R script called run_analysis.R that does the following. 
-
+# You should create one R script called run_analysis.R that does the following.
 # Merges the training and the test sets to create one data set.
-# Extracts only the measurements on the mean and standard deviation for each measurement. 
+# Extracts only the measurements on the mean and standard deviation for each measurement.
 # Uses descriptive activity names to name the activities in the data set
-# Appropriately labels the data set with descriptive activity names. 
-# Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+# Appropriately labels the data set with descriptive activity names.
+# Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 #change working directory
-setwd("/Users/ann_jinette/UCI_HAR_Dataset")
-
+setwd("/Users/ann.hess/Desktop/files/data/UCI_HAR_Dataset")
 readData <- function(fname_suffix, path_prefix) {
-fpath <- file.path(path_prefix, paste0("y_", fname_suffix, ".txt"))
-y_data <- read.table(fpath, header=F, col.names=c("ActivityID"))
-  
-fpath <- file.path(path_prefix, paste0("subject_", fname_suffix, ".txt"))
-subject_data <- read.table(fpath, header=F, col.names=c("SubjectID"))
-  
-# read the column names
-data_cols <- read.table("features.txt", header=F, as.is=T, col.names=c("MeasureID", "MeasureName"))
-  
-# read the X data file
-fpath <- file.path(path_prefix, paste0("X_", fname_suffix, ".txt"))
-data <- read.table(fpath, header=F, col.names=data_cols$MeasureName)
-  
-# names of subset columns required
-subset_data_cols <- grep(".*mean\\(\\)|.*std\\(\\)", data_cols$MeasureName)
-  
-# subset the data (done early to save memory)
-data <- data[,subset_data_cols]
-  
-# append the activity id and subject id columns
-data$ActivityID <- y_data$ActivityID
-data$SubjectID <- subject_data$SubjectID
-  
-# return the data
-data
+        fpath <- file.path(path_prefix, paste0("y_", fname_suffix, ".txt"))
+        y_data <- read.table(fpath, header=F, col.names=c("ActivityID"))
+        fpath <- file.path(path_prefix, paste0("subject_", fname_suffix, ".txt"))
+        subject_data <- read.table(fpath, header=F, col.names=c("SubjectID"))
+        # read the column names
+        data_cols <- read.table("features.txt", header=F, as.is=T, col.names=c("MeasureID", "MeasureName"))
+        # read the X data file
+        fpath <- file.path(path_prefix, paste0("X_", fname_suffix, ".txt"))
+        data <- read.table(fpath, header=F, col.names=data_cols$MeasureName)
+        # names of subset columns required
+        subset_data_cols <- grep(".*mean\\(\\)|.*std\\(\\)", data_cols$MeasureName)
+        # subset the data (done early to save memory)
+        data <- data[,subset_data_cols]
+        # append the activity id and subject id columns
+        data$ActivityID <- y_data$ActivityID
+        data$SubjectID <- subject_data$SubjectID
+        # return the data
+        data
 }
-
 # read test data set, in a folder named "test", and data file names suffixed with "test"
 readTestData <- function() {
-readData("test", "test")
+        readData("test", "test")
 }
-
 # read test data set, in a folder named "train", and data file names suffixed with "train"
 readTrainData <- function() {
-readData("train", "train")
+        readData("train", "train")
 }
-
 # Merge both train and test data sets
 # Also make the column names nicer
 mergeData <- function() {
-  data <- rbind(readTestData(), readTrainData())
-  cnames <- colnames(data)
-  cnames <- gsub("\\.+mean\\.+", cnames, replacement="Mean")
-  cnames <- gsub("\\.+std\\.+", cnames, replacement="Std")
-  colnames(data) <- cnames
-  data
+        data <- rbind(readTestData(), readTrainData())
+        cnames <- colnames(data)
+        cnames <- gsub("\\.+mean\\.+", cnames, replacement="Mean")
+        cnames <- gsub("\\.+std\\.+", cnames, replacement="Std")
+        colnames(data) <- cnames
+        data
 }
-
 # Add the activity names as another column
 applyActivityLabel <- function(data) {
-  activity_labels <- read.table("activity_labels.txt", header=F, as.is=T, col.names=c("ActivityID", "ActivityName"))
-  activity_labels$ActivityName <- as.factor(activity_labels$ActivityName)
-  data_labeled <- merge(data, activity_labels)
-  data_labeled
+        activity_labels <- read.table("activity_labels.txt", header=F, as.is=T, col.names=c("ActivityID", "ActivityName"))
+        activity_labels$ActivityName <- as.factor(activity_labels$ActivityName)
+        data_labeled <- merge(data, activity_labels)
+        data_labeled
 }
-
 # Combine training and test data sets and add the activity label as another column
 getMergedLabeledData <- function() {
-  applyActivityLabel(mergeData())
+        applyActivityLabel(mergeData())
 }
-
 # Create a tidy data set that has the average of each variable for each activity and each subject.
 getTidyData <- function(merged_labeled_data) {
-  library(reshape2)
-  
-# melt the dataset
-id_vars = c("ActivityID", "ActivityName", "SubjectID")
-measure_vars = setdiff(colnames(merged_labeled_data), id_vars)
-melted_data <- melt(merged_labeled_data, id=id_vars, measure.vars=measure_vars)
-  
-# recast
-dcast(melted_data, ActivityName + SubjectID ~ variable, mean)
+        library(reshape2)
+        # melt the dataset
+        id_vars = c("ActivityID", "ActivityName", "SubjectID")
+        measure_vars = setdiff(colnames(merged_labeled_data), id_vars)
+        melted_data <- melt(merged_labeled_data, id=id_vars, measure.vars=measure_vars)
+        # recast
+        dcast(melted_data, ActivityName + SubjectID ~ variable, mean)
 }
-
 # Create the tidy data set and save it on to the named file
 createTidyDataFile <- function(fname) {
-  tidy_data <- getTidyData(getMergedLabeledData())
-  write.table(tidy_data, fname)
+        tidy_data <- getTidyData(getMergedLabeledData())
+        write.table(tidy_data, fname)
 }
-
 print("Assuming data files from the \"UCI HAR Dataset\" are availale in the current directory with the same structure as in the downloaded archive.")
 print(" Refer Data:")
 print(" archive: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip")
@@ -99,3 +79,5 @@ print(" description: dataset: http://archive.ics.uci.edu/ml/datasets/Human+Activ
 print("Creating tidy dataset as tidy_dataset.txt...")
 createTidyDataFile("tidy_dataset.txt")
 print("Done.")
+
+
